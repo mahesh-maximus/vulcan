bc4f8ed5e99ba4963bf9b10f3971d0f9338bdd49
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2022-05-09 01:39:18 +0530

    Create 4_start-guest-machine.sh

---
+++ samples/run-firecracker/getting-started.md/4_start-guest-machine.sh
@@ -0,0 +1,7 @@
+curl --unix-socket /tmp/firecracker.socket -i \
+  -X PUT 'http://localhost/actions'       \
+  -H  'Accept: application/json'          \
+  -H  'Content-Type: application/json'    \
+  -d '{
+      "action_type": "InstanceStart"
+   }'
