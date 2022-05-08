ad9747e2476acc9667e3b138ffc5d3c44666d3d3
Mahesh Dharmasena <mahesh.ksl@gmail.com>
2022-05-09 01:43:42 +0530

    Create 5_update-machine-config.sh

---
+++ samples/run-firecracker/getting-started.md/5_update-machine-config.sh
@@ -0,0 +1,8 @@
+curl --unix-socket /tmp/firecracker.socket -i  \
+  -X PUT 'http://localhost/machine-config' \
+  -H 'Accept: application/json'            \
+  -H 'Content-Type: application/json'      \
+  -d '{
+      "vcpu_count": 2,
+      "mem_size_mib": 512
+  }'
